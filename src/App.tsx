0f4b17c5dc8fe2ab0f4b9ef27dab6057bc42c593
fabricemu <fabthemachine@gmail.com>
2025-05-27 09:47:54 +0200

    Ads

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Education from "./pages/Education.tsx";
@@ -12,7 +11,6 @@ export default function App() {
             <Layout>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
                     <Route path="/education" element={<Education/>}/>
                     <Route path="/projects" element={<Projects/>}/>
                     <Route path="/contact" element={<Contact/>}/>
